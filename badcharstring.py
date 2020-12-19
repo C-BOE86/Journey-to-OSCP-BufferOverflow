6a37a6a143a4e649b40dcdeadd3af6ff6dbfe330
C-BOE <17278637+C-BOE86@users.noreply.github.com>
2020-12-19 16:41:03 -0500

    Create badcharstring.py

---
+++ badcharstring.py
@@ -0,0 +1,5 @@
+from __future__ import print_functionlistRem = "".split("\\x")
+for x in range(1, 256):
+    if "{:02x}".format(x) not in listRem:
+        print("\\x" + "{:02x}".format(x), end='')
+print()
